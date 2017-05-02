0524a161b6082745e985ab40f21b8b72211ba35a
Serghei Gherstioga <sgherstioga@tkmd-118.tacitknowledge.com>
2017-05-02 17:07:42 +0300

    task_14 start

--- Task_14.py
+++ Task_14.py
@@ -1,3 +1,14 @@
+"""Write a program or function that takes a list
+and returns a new list that contains all the elements
+of the first list minus all the duplicates.
+Extras:
+Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+"""
 
 
-"""Additional testing"""
+names = ["Michele", "Robin", "Sara", "Michele"]
+print names
+names = set(names)
+names = list(names)
+print(names)
